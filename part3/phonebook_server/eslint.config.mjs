b1ed563a3c2e6af3de177465d6fa113303e19799
Melchor Ruiz <ruizgonzalezmelchor3@gmail.com>
2024-06-10 16:02:21 -0600

    chore: Add ESLint configuration for phonebook server

---
+++ part3/phonebook_server/eslint.config.mjs
@@ -0,0 +1,46 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import stylisticJs from '@stylistic/eslint-plugin-js'
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+      ],
+      'no-console': 0
+    }
+  },
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    'extends': 'eslint:recommended',
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
+    }
+  }
+];
--- part3/phonebook_server/models/person.js
+++ part3/phonebook_server/models/person.js
@@ -22,6 +22,12 @@ const personSchema = new mongoose.Schema({
     },
     "number": {
         type: String,
+        validate: {
+            validator: (v) => {
+                return /^\d{2}-\d{2}-\d{4}$/.test(v) || /^\d{3}-\d{5}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
         required: true,
     }
 })
